6f5e60b00aabd5254b1370291374e1db5693901c
Devin Warrick <dev.warr@gmail.com>
2020-04-20 22:11:30 -0500

    Added FooterCategory

---
+++ ecommerce/src/components/Footer/FooterCategory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function FooterCategory({ category }) {
+	return (
+		<li class="p-b-9">
+			<a href="#" class="s-text7">
+				{category}
+			</a>
+		</li>
+	);
+}
